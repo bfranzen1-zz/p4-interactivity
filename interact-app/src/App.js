af68d55a61e07d622517e31f5a30dfa1aa8fd832
Blake-Franzen <bfranzen@uw.edu>
2018-02-25 16:23:39 -0800

    adding component files

--- interact-app/src/App.js
+++ interact-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
 class App extends Component {
   render() {
@@ -17,5 +18,12 @@ class App extends Component {
     );
   }
 }
+
+//Pages:
+//User Recipes
+//User Settings/account
+//recipes
+//signin/up/out
+//Home page (informational/how to use site)
 
 export default App;
