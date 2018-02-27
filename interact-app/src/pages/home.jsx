da044cc38af7e5d3a135d2ec9ef10f8af9c14ee9
bfranzen1 <bef6823@gmail.com>
2018-02-27 10:43:05 -0800

    style homepage/pass user to pages

--- interact-app/src/pages/home.jsx
+++ interact-app/src/pages/home.jsx
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 
 class Home extends Component {
     render() {
+        console.log(this.props.user);
         return (
-            <h1>Home Page</h1>
+            <h1>Welcome {this.props.user.displayName}!</h1>
         )
     }
 }
