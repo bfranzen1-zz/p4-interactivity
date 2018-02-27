94bdf329a4cd40bc7a02fb99fe60f7fbc365b914
Jacob Matray <jmatray@uw.edu>
2018-02-27 10:43:50 -0800

    Allow recipes to be added to firebase

--- interact-app/src/pages/userRecipes.jsx
+++ interact-app/src/pages/userRecipes.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 
 class userRecipes extends Component {
     constructor() {
         super();
         this.state = {
             isHidden: true,
-            creating: false
+            creating: false,
+            recipes: [],
+            name: '',
+            imgLink: '',
+            ingredients: '',
+            steps: ''
         };
     }
 
@@ -17,13 +23,41 @@ class userRecipes extends Component {
         });
     }
 
+    componentDidMount() {
+        this.requestRef = firebase.database().ref('recipes');
+
+        this.requestRef.on('value', (snapshot) => {
+            let recipes = snapshot.val();
+            this.setState({ recipes: recipes});
+        });
+    }
+
+    updateForm(event) {
+        let val = event.target.value;
+        let field = event.target.name;
+        let change = {};
+        change[field] = val;
+        this.setState(change);
+    }
+
+    addRecipe() {
+        let recipe = {
+            name: this.state.name,
+            imgLink: this.state.imgLink,
+            ingredients: this.state.ingredients,
+            steps: this.state.steps
+        };
+        this.toggleHidden();
+        this.requestRef.push(recipe);
+    }
+
     render() {
         return (
             <div>
                 <h1>My Recipes Page</h1>
                 <div>
                     <button onClick={() => this.toggleHidden()} className={this.state.creating ? "btn btn-warning" : "btn btn-primary"}>{this.state.creating ? "Cancel" : "Create New Recipe"}</button>
-                    {!this.state.isHidden && <RecipeForm />}
+                    {!this.state.isHidden && <RecipeForm addRecipe={() => this.addRecipe()} updateForm={(event) => this.updateForm(event)}/>}
                 </div>
             </div>
         )
@@ -34,25 +68,26 @@ export default userRecipes
 class RecipeForm extends Component {
     render() {
         return (
-            <form action="/my-handling-form-page" method="post">
+            <div>
                 <div className="form-group">
                     <label htmlFor="reicpeName">Recipe Name:</label>
-                    <input type="text" className="form-control" id="recipeName" name="recipe_name" />
+                    <input type="text" className="form-control" id="recipeName" name="name" onChange={(event) => {this.props.updateForm(event)} } />
                 </div>
                 <div className="form-group">
                     <label htmlFor="img">Image URL:</label>
-                    <input type="url" className="form-control" id="img" name="recipe_image"></input>
+                    <input type="url" className="form-control" id="img" name="image" onChange={(event) => {this.props.updateForm(event)} } />
                 </div>
                 <div className="form-group">
                     <label htmlFor="ingredients">Ingredients List:</label>
-                    <textarea id="ingredients" className="form-control" name="recipe_ingredients"></textarea>
+                    <textarea id="ingredients" className="form-control" name="ingredients" onChange={(event) => {this.props.updateForm(event)} }></textarea>
                 </div>
                 <div className="form-group">
                     <label htmlFor="steps">Recipe steps:</label>
-                    <textarea id="steps" className="form-control" name="recipe_steps"></textarea>
+                    <textarea id="steps" className="form-control" name="steps" onChange={(event) => {this.props.updateForm(event)} }></textarea>
                 </div>
-                <button type="submit" className="btn btn-primary">Post Your Recipe!</button>
-            </form>
+                <button className="btn btn-primary" onClick={() => this.props.addRecipe()}>Post Your Recipe!</button>
+            </div>
         )
     }
 }
+
